dd6fa321e10ed9b13be2e8d3f02438bdbed30075
azumbado11 <gv11zumbado@gmail.com>
2024-06-17 15:43:34 -0600

    Deleted Comments

--- src/app/page.js
+++ src/app/page.js
@@ -26,12 +26,6 @@ export default async function Home() {
 }
 
 /* 
-New Arrivals / ShopbySport / Popular son un contenedor que acepta <Items> pensar como componetizarlo
-Carrousel = ({title, itemData}) => {}
-
-*/
-
-/* 
 Routes: 
   "/"                     Home
   "/store"                Store              Fetch(Items)
@@ -42,46 +36,4 @@ Routes:
   /store&sport=""         Filter by Sport
   /store&category=""      Filter by Category
   "/store[1]"             Show Full Item     Fetch(Item:ID)
-
-  not-found.jsx en el fichero /app para crear la página de error 404 
-  Server Fetching https://www.youtube.com/watch?v=jMy4pVZMyLM 1:14:00
-
-  Para el carrito, habría que hacer 
-  post   * añadir item
-  patch  * modificar item (cantidad) 
-  delete * eliminar del carrito
-  get    * traer el carrito del usuario, aunque para ello habría que hacer un registro previo
-
-1.0
--. Arreglar las imagenes
--. UI de los filtros                           (✓)
--. Carousel funcional
--. Custom Hook de filtros                      (X)
--. Custom hook de itemContext y cartContext    (X)
--. Página de la bolsa de compra                (✓)
--. API con endpoints                           (✓)
--. Enrutado dinámico                           (✓)
--. Responsive 
-
-2.0
--.Agregar para link roto intencional                                                 (✓)
--.Añadir req querys con su respectivo filtro de la API para New arrivals y Sale      (✓)
--.Añadir UI que muestre New Arrivals y Sale en /store                                (✓)
--.Carousel funcional                                                                 (✓)
--.Hacer las descripciones de los items y el apartado de información del home         (✓)
--.Añadir uno o dos productos para cada deporte                                       (✓)
--.Arreglar y añadir imagenes                                                         (✓)
--.Mejorar descripciones del proyecto                                                 (✓)
--.Añadir funcionalidad para borrar todo el contenido del carrito                     (✓)
--.Añadir lo de cantidad, si hay dos items duplicados, que cuenten como uno           (✓)
--.Responsive                                                                         (✓)
--.Cambiar la UI de los filtros en /store a horizontal                                (✓)
-
-
-Tablet
-@media (max-width: 895px){}
-
-Mobile
-@media (max-width: 700px){}
-
 */
